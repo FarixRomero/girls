2071de934d8fa0ce1e6a64763d977a8cb5add962
FarixRomeroCode <a20193387@pucp.edu.pe>
2021-09-13 08:30:25 -0500

    integracion de jaun

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\FileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\AdvertisementController;
+use App\Models\Service;
+use GuzzleHttp\Middleware;
+use App\Http\Middleware\CheckAdvertisementCreated;
 /*
 |--------------------------------------------------------------------------
 | API Routes
